227d92f5136c3528f2fc018e4d99cb45ca2903ff
Varshini-2003 <138800152+Varshini-2003@users.noreply.github.com>
2025-04-16 16:17:49 +0530

    Delete LoanManagement directory

--- LoanManagement/exception/invalid_loan_exception.py
+++
@@ -1,5 +0,0 @@
-class InvalidLoanException(Exception):
-    def __init__(self, message="Invalid Loan ID provided. Loan not found in the database."):
-        self.message = message
-        super().__init__(self.message)
-
